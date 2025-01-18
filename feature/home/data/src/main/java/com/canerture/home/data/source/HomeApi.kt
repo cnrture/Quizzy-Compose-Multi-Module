665f4005d6418c9987526e64d02fe962b4d85640
Caner Türe <cnrture@gmail.com>
2025-01-18 20:13:50 +0300

    remove search endpoint from home

--- feature/home/data/src/main/java/com/canerture/home/data/source/HomeApi.kt
+++ feature/home/data/src/main/java/com/canerture/home/data/source/HomeApi.kt
@@ -2,12 +2,10 @@ package com.canerture.home.data.source
 
 import com.canerture.home.data.common.Constants.CATEGORIES
 import com.canerture.home.data.common.Constants.QUIZZES
-import com.canerture.home.data.common.Constants.SEARCH
 import com.canerture.home.data.model.CategoriesResponse
 import com.canerture.home.data.model.PopularQuizzesResponse
 import com.canerture.network.model.BaseResponse
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface HomeApi {
     @GET(CATEGORIES)
@@ -15,7 +13,4 @@ interface HomeApi {
 
     @GET(QUIZZES)
     suspend fun getQuizzes(): BaseResponse<List<PopularQuizzesResponse>?>
-
-    @GET(SEARCH)
-    suspend fun search(@Query("query") query: String): BaseResponse<List<PopularQuizzesResponse>?>
 }
