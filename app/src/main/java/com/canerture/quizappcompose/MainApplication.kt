792de4234e2def94beb6546ea1260ffbd4333350
candroid <cnrture@gmail.com>
2025-07-21 20:58:41 +0300

    feat: sample

--- app/src/main/java/com/canerture/quizappcompose/MainApplication.kt
+++ app/src/main/java/com/canerture/quizappcompose/MainApplication.kt
@@ -2,6 +2,5 @@ package com.canerture.quizappcompose
 
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
-
 @HiltAndroidApp
 class MainApplication : MultiDexApplication()
